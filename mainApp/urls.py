b5426b73a745761fff49760047d6a98227cdedfd
wingxel <wingxel@gmail.com>
2023-05-14 19:13:50 +0300

    removed unnecessary file from git tracking

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,15 +3,15 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('index.html/', views.index),
-    path('activity.html/', views.activity),
-    path('create-room.html/', views.create_room),
-    path('delete.html/', views.delete),
-    path('edit-user.html/', views.edit_user),
-    path('login.html/', views.login),
-    path('profile.html/', views.profile),
-    path('room.html/', views.room),
-    path('settings.html/', views.settings),
-    path('signup.html/', views.signup),
-    path('topics.html/', views.topics)
+    path('index/', views.index),
+    path('activity/', views.activity),
+    path('create-room/', views.create_room),
+    path('delete/', views.delete),
+    path('edit-user/', views.edit_user),
+    path('login/', views.login),
+    path('profile/', views.profile),
+    path('room/', views.room),
+    path('settings/', views.settings),
+    path('signup/', views.signup),
+    path('topics/', views.topics)
 ]
