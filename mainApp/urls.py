57ef546c15df809b096ebe56b6a0182bf4b21dec
wingxel <wingxel@gmail.com>
2023-05-14 20:22:29 +0300

    Fixed url errors

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,17 +1,19 @@
+from unicodedata import name
 from . import views
 from django.urls import path
 
 
 urlpatterns = [
-    path('index/', views.index),
-    path('activity/', views.activity),
-    path('create-room/', views.create_room),
-    path('delete/', views.delete),
-    path('edit-user/', views.edit_user),
-    path('login/', views.login),
-    path('profile/', views.profile),
-    path('room/', views.room),
-    path('settings/', views.settings),
-    path('signup/', views.signup),
-    path('topics/', views.topics)
+    path('', views.index, name="home"),
+    path('index/', views.index, name="index"),
+    path('activity/', views.activity, name="activity"),
+    path('create-room/', views.create_room, name="create-room"),
+    path('delete/', views.delete, name="delete"),
+    path('edit-user/', views.edit_user, name="edit-user"),
+    path('login/', views.login, name="login"),
+    path('profile/', views.profile, name="profile"),
+    path('room/', views.room, name="room"),
+    path('settings/', views.settings, name="settings"),
+    path('signup/', views.signup, name="signup"),
+    path('topics/', views.topics, name="topics")
 ]
