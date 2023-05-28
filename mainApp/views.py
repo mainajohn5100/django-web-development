ec47d495e07ce61ad684da21e2293cb27f954a60
wingxel <wingxel@gmail.com>
2023-05-28 11:19:20 +0300

    Removed theme and assets folders

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,35 +2,35 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request) -> None:
-    return render(request, "mainApp/theme/index.html")
+    return render(request, "mainApp/index.html")
 
 
 def activity(request) -> None:
-    return render(request, "mainApp/theme/activity.html")
+    return render(request, "mainApp/activity.html")
     
 def create_room(request) -> None:
-    return render(request, "mainApp/theme/create-room.html")
+    return render(request, "mainApp/create-room.html")
 
 def delete(request) -> None:
-    return render(request, "mainApp/theme/delete.html")
+    return render(request, "mainApp/delete.html")
 
 def edit_user(request) -> None:
-    return render(request, "mainApp/theme/edit-user.html")
+    return render(request, "mainApp/edit-user.html")
 
 def login(request) -> None:
-    return render(request, "mainApp/theme/login.html")
+    return render(request, "mainApp/login.html")
 
 def profile(request) -> None:
-    return render(request, "mainApp/theme/profile.html")
+    return render(request, "mainApp/profile.html")
 
 def room(request) -> None:
-    return render(request, "mainApp/theme/room.html")
+    return render(request, "mainApp/room.html")
 
 def settings(request) -> None:
-    return render(request, "mainApp/theme/settings.html")
+    return render(request, "mainApp/settings.html")
 
 def signup(request) -> None:
-    return render(request, "mainApp/theme/signup.html")
+    return render(request, "mainApp/signup.html")
 
 def topics(request) -> None:
-    return render(request, "mainApp/theme/topics.html")
+    return render(request, "mainApp/topics.html")
